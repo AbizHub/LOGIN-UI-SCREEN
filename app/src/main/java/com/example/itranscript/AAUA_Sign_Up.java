6a4a2423ebd775cbfc2d0e7104df8497da56cb25
root <omoniyiabaiola19@gmail.com>
2020-07-14 03:14:14 +0100

    UI SCREEN

---
+++ app/src/main/java/com/example/itranscript/AAUA_Sign_Up.java
@@ -0,0 +1,14 @@
+package com.example.itranscript;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AAUA_Sign_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_a_u_a__sign__up);
+    }
+}
---
+++ app/src/main/java/com/example/itranscript/aaua_Transcript_login.java
@@ -0,0 +1,14 @@
+package com.example.itranscript;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aaua_Transcript_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aaua__transcript_login);
+    }
+}
